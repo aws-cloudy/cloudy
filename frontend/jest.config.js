cb4276ccefca8fe2775ee76e65b61a05e705cd7d
김내림 <naerim1119@naver.com>
2024-04-17 14:33:41 +0900

    [FE-SETTING] feat: jest, rtl setting #4

---
+++ frontend/jest.config.js
@@ -0,0 +1,13 @@
+const nextJest = require('next/jest')
+
+// Next.js 에게 경로를 제공
+const createJestConfig = nextJest({ dir: './' })
+
+// jest 에서 설정하고 싶은 옵션을 여기서 작성하면 된다.
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+// createJestConfig는 next/jest가 비동기식인 Next.js 구성을 로드할 수 있도록 이러한 방식으로 내보내짐
+module.exports = createJestConfig(customJestConfig)
