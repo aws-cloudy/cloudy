ef8346e42662d0c8b13fbaf3f319ce626abd8399
김한주 <the_1_week@naver.com>
2024-04-17 15:44:37 +0900

    [Infra] add : catalog service 프로젝트 세팅 #5

---
+++ backend/cloudy/src/test/java/com/s207/cloudy/domain/health/controller/HealthControllerTest.java
@@ -0,0 +1,28 @@
+package com.s207.cloudy.domain.health.controller;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HealthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    void 관리자는_헬스체크_api로_서버상태를_확인할_수_있다()throws Exception{
+        mockMvc.perform(get("/health"))
+                .andExpect(status().isOk());
+    }
+
+
+}
