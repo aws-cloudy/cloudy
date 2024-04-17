c07464f22def7afbb32e7f99a9ddf6882a66fb9d
the_1_week <the_1_week@naver.com>
2024-04-17 15:24:25 +0900

    [Infra] feat: 프로젝트 생성 (#5)

---
+++ backend/cloudy/src/main/java/com/s207/cloudy/CloudyApplication.java
@@ -0,0 +1,13 @@
+package com.s207.cloudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudyApplication.class, args);
+	}
+
+}
