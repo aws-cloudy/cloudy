e3b7f893028e48e867c5a8e9b4735d0854da2043
김내림 <naerim1119@naver.com>
2024-04-18 14:33:48 +0900

    [FE-COMPONENT] feat : header mobile version #10

--- frontend/components/common/Header/index.tsx
+++ frontend/components/common/Header/index.tsx
@@ -1,34 +1,70 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import styles from './Header.module.scss'
 import Link from 'next/link'
 import { MdOutlineLanguage } from 'react-icons/md'
+import { MdMenu } from 'react-icons/md'
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <header className={styles.header}>
-      <div className={styles.headerWrap}>
-        <Link href="/" className={styles.logoWrap}>
-          <img className={styles.logo} src="/img/logo.png" alt="logo" />
-        </Link>
-        <div className={styles.menuList}>
-          <Link href="/learning" className={styles.menuItem}>
-            학습
+    <>
+      <header className={styles.header}>
+        <div className={styles.headerWrap}>
+          <Link href="/" className={styles.logoWrap}>
+            <img className={styles.logo} src="/img/logo.png" alt="logo" />
           </Link>
-          <Link href="/roadmap" className={styles.menuItem}>
-            로드맵
-          </Link>
-          <Link href="/community" className={styles.menuItem}>
-            커뮤니티
-          </Link>
+          <div className={styles.menuList}>
+            <Link href="/learning" className={styles.menuItem}>
+              학습
+            </Link>
+            <Link href="/roadmap" className={styles.menuItem}>
+              로드맵
+            </Link>
+            <Link href="/community" className={styles.menuItem}>
+              커뮤니티
+            </Link>
+          </div>
+          <div className={styles.RightWrap}>
+            <Link href="/learning" className={styles.menuItem}>
+              로그인
+            </Link>
+            <MdOutlineLanguage size={24} className="languageIcon" />
+          </div>
         </div>
-        <div className={styles.RightWrap}>
-          <Link href="/learning" className={styles.menuItem}>
-            로그인
-          </Link>
-          <MdOutlineLanguage width="50px" size={24} className="languageIcon" />
-        </div>
-      </div>
-    </header>
+      </header>
+      {/* mobile */}
+      <>
+        <header className={styles.headerMobile}>
+          <div className={styles.headerMobileWrap}>
+            <Link href="/" className={styles.logoWrap}>
+              <img className={styles.logo} src="/img/logo.png" alt="logo" />
+            </Link>
+            <div className={styles.MenuIconWrap} onClick={() => setIsOpen(!isOpen)}>
+              <MdMenu size={24} />
+            </div>
+          </div>
+          {isOpen && (
+            <div className={styles.mobileMenuList} role="presentation">
+              <Link href="/learning" className={styles.mobileMenuItem}>
+                학습
+              </Link>
+              <Link href="/roadmap" className={styles.mobileMenuItem}>
+                로드맵
+              </Link>
+              <Link href="/community" className={styles.mobileMenuItem}>
+                커뮤니티
+              </Link>
+              <Link href="/community" className={styles.mobileMenuItem}>
+                로그인
+              </Link>
+            </div>
+          )}
+        </header>
+      </>
+    </>
   )
 }
 
