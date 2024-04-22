e8ea62b04be961dde3a8ab501feded93d0600316
김내림 <naerim1119@naver.com>
2024-04-22 16:28:28 +0900

    [FE-COMPONENT] fix: storybook args 추가 #14

--- frontend/components/learning/LearningInput/LearningInput.stories.tsx
+++ frontend/components/learning/LearningInput/LearningInput.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import LearningInput from './'
+import { fn } from '@storybook/test'
 
 const meta = {
   title: 'learning/LearningInput',
@@ -9,6 +10,7 @@ const meta = {
   },
 
   tags: ['autodocs'],
+  args: { value: 'value', setValue: fn(), keyword: 'keyword', setKeyword: fn() },
 } satisfies Meta<typeof LearningInput>
 
 export default meta
