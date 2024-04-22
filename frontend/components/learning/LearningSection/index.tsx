b4c04554fc91319c82a7d6706a73da8d987ea2f3
김내림 <naerim1119@naver.com>
2024-04-22 15:47:06 +0900

    [FE-COMPONENT] feat: 추천검색어 visible 조건 추가 #14

--- frontend/components/learning/LearningSection/index.tsx
+++ frontend/components/learning/LearningSection/index.tsx
@@ -6,12 +6,13 @@ import LearningInput from '../LearningInput'
 import LearningSearchList from '../LearningSearchList'
 
 const LearningSection = () => {
-  const [value, setValue] = useState<string>('')
+  const [value, setValue] = useState<string>('') // input value
+  const [keyword, setKeyword] = useState<string>('') // 검색어
 
   return (
     <section className={styles.section}>
-      <LearningInput value={value} setValue={setValue} />
-      {value && <LearningSearchList />}
+      <LearningInput value={value} setValue={setValue} setKeyword={setKeyword} />
+      {value && keyword === '' && <LearningSearchList />}
       <div>input area</div>
     </section>
   )
--- frontend/types/learning.ts
+++ frontend/types/learning.ts
@@ -3,4 +3,5 @@ import { Dispatch, SetStateAction } from 'react'
 export interface ILearningInput {
   value: string
   setValue: Dispatch<SetStateAction<string>>
+  setKeyword: Dispatch<SetStateAction<string>>
 }
