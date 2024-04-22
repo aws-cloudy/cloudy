876ff45221690a8392da3f0ae17413fba834e2bf
yihoney <109622@naver.com>
2024-04-22 16:42:34 +0900

    feat: 로드맵 전체 조회 응답 dto 생성

---
+++ backend/cloudy/src/main/java/com/s207/cloudy/domain/roadmapgroup/roadmap/dto/RoadmapListRes.java
@@ -0,0 +1,16 @@
+package com.s207.cloudy.domain.roadmapgroup.roadmap.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class RoadmapListRes {
+    private int roadmapId;
+    private String title;
+    private String thumbnail;
+    private String service;
+    private String job;
+    private String summary;
+    private int commentsCnt;
+}
