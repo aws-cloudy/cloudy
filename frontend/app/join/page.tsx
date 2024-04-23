5e551d50dad8cd791f88dc44bece839e1bc13bf0
전상혁 <tkdgur0826@gmail.com>
2024-04-23 12:07:55 +0900

    [FE-PAGE] feat: 정보등록 컴포넌트 UI 디자인 #21

---
+++ frontend/app/join/page.tsx
@@ -0,0 +1,14 @@
+import RegistInfo from '@/components/registInfomation'
+import styles from './page.module.scss'
+
+const joinPage = () => {
+  return (
+    <>
+      <section className={styles.section}>
+        <RegistInfo />
+      </section>
+    </>
+  )
+}
+
+export default joinPage
---
+++ frontend/components/registInfomation/index.tsx
@@ -0,0 +1,62 @@
+'use client'
+import Select from 'react-select'
+import styles from './indext.module.scss'
+import '@/styles/theme.scss'
+export default function RegistInfo() {
+  const options = [
+    { value: 'Data Scientist', label: 'Data Scientist' },
+    { value: 'Data2', label: 'Data2' },
+    { value: 'Data3', label: 'Data3' },
+    { value: 'Data4', label: 'Data4' },
+    { value: 'Data5', label: 'Data5' },
+  ]
+
+  const customStyles = {
+    control: (base: any) => ({
+      ...base,
+      height: 50,
+      minheight: 50,
+    }),
+  }
+
+  return (
+    <div className={styles.section}>
+      <div className={styles.title}>내 정보 등록</div>
+      <div className={styles.grayText}>강의 추천을 위해 추가 정보를 입력해주세요.</div>
+      <div className={styles.line} />
+      <Select
+        options={options}
+        className={styles.select}
+        placeholder="직무"
+        theme={theme => ({
+          ...theme,
+          colors: {
+            ...theme.colors,
+            primary25: '#ff9900',
+            primary75: 'black',
+            primary: 'black',
+          },
+        })}
+        styles={customStyles}
+      />
+      <Select
+        options={options}
+        className={styles.select}
+        placeholder="관심 서비스"
+        theme={theme => ({
+          ...theme,
+          colors: {
+            ...theme.colors,
+            primary25: '#ff9900',
+            primary75: 'black',
+            primary: 'black',
+          },
+        })}
+        styles={customStyles}
+      />
+      <div className={styles.blackText}>등록한 정보는 마이페이지에서 변경할 수 있습니다.</div>
+
+      <button className={styles.submit}>확인</button>
+    </div>
+  )
+}
