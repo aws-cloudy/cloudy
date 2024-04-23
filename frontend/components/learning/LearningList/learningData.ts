ffce1c8fc79eb05c952ed1ca44c3fa78b1dc6078
김내림 <naerim1119@naver.com>
2024-04-23 09:59:39 +0900

    [FE-COMPONENT] feat: 학습 더미 데이터 생성 #18

---
+++ frontend/components/learning/LearningList/learningData.ts
@@ -0,0 +1,102 @@
+export const learningData = [
+  {
+    id: 0,
+    title: 'AWS Hadoop Fundamentals (Korean)',
+    difficulty: 'Fundamental',
+    duration: '5h 30m',
+    desc: '',
+    summary:
+      '효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음 효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음 효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음',
+    thumbnail: '/img/learning/1.png',
+    link: 'https://www.naver.com',
+    type: 'Digital Course',
+    job: [
+      { id: 0, name: 'data scientist' },
+      { id: 1, name: 'developer' },
+    ],
+    service: [
+      { id: 0, name: '클라우드' },
+      { id: 1, name: 'DB' },
+    ],
+  },
+  {
+    id: 1,
+    title: 'Sustainability Transformation with AWS (Korean)',
+    difficulty: 'Fundamental',
+    duration: '45m',
+    desc: '',
+    summary:
+      '효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음 효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음 효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음',
+    thumbnail: '/img/learning/2.png',
+    link: 'https://www.naver.com',
+    type: 'Digital Course',
+    job: [
+      { id: 0, name: 'data scientist' },
+      { id: 1, name: 'developer' },
+    ],
+    service: [
+      { id: 0, name: '클라우드' },
+      { id: 1, name: 'DB' },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Getting Started with WAS for Games - Part 1 (Korean)',
+    difficulty: 'Fundamental',
+    duration: '30m',
+    desc: '',
+    summary:
+      '효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음 효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음 효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음',
+    thumbnail: '/img/learning/3.png',
+    link: 'https://www.naver.com',
+    type: 'Digital Course',
+    job: [
+      { id: 0, name: 'data scientist' },
+      { id: 1, name: 'developer' },
+    ],
+    service: [
+      { id: 0, name: '클라우드' },
+      { id: 1, name: 'DB' },
+    ],
+  },
+  {
+    id: 3,
+    title: 'AWS Compute Services Overview (Korean)',
+    difficulty: 'Intermediate',
+    duration: '1h 30m',
+    desc: '',
+    summary:
+      '효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음 효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음 효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음',
+    thumbnail: '/img/learning/4.png',
+    link: 'https://www.naver.com',
+    type: 'Digital Course',
+    job: [
+      { id: 0, name: 'data scientist' },
+      { id: 1, name: 'developer' },
+    ],
+    service: [
+      { id: 0, name: '클라우드' },
+      { id: 1, name: 'DB' },
+    ],
+  },
+  {
+    id: 4,
+    title: 'Video Streaming Concepts: AWS Media Services (Korean)',
+    difficulty: 'Advanced',
+    duration: '1h 45m',
+    desc: '',
+    summary:
+      '효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음 효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음 효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음',
+    thumbnail: '/img/learning/5.png',
+    link: 'https://www.naver.com',
+    type: 'Digital Course',
+    job: [
+      { id: 0, name: 'data scientist' },
+      { id: 1, name: 'developer' },
+    ],
+    service: [
+      { id: 0, name: '클라우드' },
+      { id: 1, name: 'DB' },
+    ],
+  },
+]
