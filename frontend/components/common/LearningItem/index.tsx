7191f4248ffcf7912b552fa7e54bf1c839887a41
김내림 <naerim1119@naver.com>
2024-04-23 11:41:06 +0900

    [FE-COMPONENT] feat: 강의 난이도 뱃지 생성 #18

--- frontend/components/common/LearningItem/index.tsx
+++ frontend/components/common/LearningItem/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import styles from './LearningItem.module.scss'
 import { ILearningItem } from '@/types/learning'
+import { getDifficulty } from '@/utils/getDifficulty'
 
 const LearningItem = (props: { item: ILearningItem }) => {
   const { item } = props
 
+  const difficulty = getDifficulty(item.difficulty)
+
   return (
     <div className={styles.container}>
-      <img src={item.thumbnail} alt={item.title} className={styles.img} />
+      <div className={styles.imgWrap}>
+        <img src={item.thumbnail} alt={item.title} className={styles.img} />
+        <div className={`${styles.badge} ${difficulty.class}`}>{difficulty.text}</div>
+      </div>
       <div className={styles.wrap}>
         <div className={styles.categoryWrap}>
           {item.service.map(service => (
