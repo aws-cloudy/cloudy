42fcd35f1ecabd26e676c1f781d609113313c082
전상혁 <tkdgur0826@gmail.com>
2024-04-23 15:44:41 +0900

    [FE-PAGE] feat: 선택한 정보 확인 추가 #21

--- frontend/components/registInfomation/index.tsx
+++ frontend/components/registInfomation/index.tsx
@@ -6,6 +6,10 @@ import ProgressBar from '../ProgressBar'
 import { useState } from 'react'
 
 export default function RegistInfo() {
+  const [selectedJob, setSelectedJob] = useState(null)
+  const [selectedService, setSelectedService] = useState(null)
+  const [confirmScreen, setConfirmScreen] = useState(false)
+
   const options = [
     { value: 'Data Scientist', label: 'Data Scientist' },
     { value: 'Data2', label: 'Data2' },
@@ -25,10 +29,45 @@ export default function RegistInfo() {
   const [currentStep, setCurrentStep] = useState(0)
   const totalSteps = 1
 
-  const handleNextStep = () => {
+  const handleJobChange = (selectedObtion: any) => setSelectedJob(selectedObtion)
+  const handleServiceChange = (selectedObtion: any) => setSelectedService(selectedObtion)
+
+  const handleConfirm = () => {
     if (currentStep < totalSteps) {
       setCurrentStep(currentStep + 1)
     }
+    setConfirmScreen(true)
+  }
+  const handleCancel = () => {
+    setCurrentStep(currentStep - 1)
+    setConfirmScreen(false)
+  }
+
+  if (confirmScreen) {
+    return (
+      <div className={styles.section}>
+        <div className={styles.title}>내 정보 등록</div>
+        <div className={styles.grayText}>강의 추천을 위해 추가 정보를 입력해주세요.</div>
+        <ProgressBar currentStep={currentStep} totalSteps={totalSteps} />
+        <div className={styles.blackText}>이하 내용으로 등록하시겠습니까?</div>
+        <div className={styles.info}>
+          <div className={styles.infoContainer}>
+            <div className={styles.infoTitle}>직무</div>
+            <div>{selectedJob ? selectedJob.label : '선택되지 않음'}</div>
+          </div>
+          <div className={styles.infoContainer}>
+            <div className={styles.infoTitle}>관심 서비스</div>
+            <div>{selectedService ? selectedService.label : '선택되지 않음'}</div>
+          </div>
+        </div>
+        <button className={styles.cancel} onClick={handleCancel}>
+          취소
+        </button>
+        <button className={styles.submit} onClick={handleConfirm}>
+          확인
+        </button>
+      </div>
+    )
   }
 
   return (
@@ -36,8 +75,11 @@ export default function RegistInfo() {
       <div className={styles.title}>내 정보 등록</div>
       <div className={styles.grayText}>강의 추천을 위해 추가 정보를 입력해주세요.</div>
       <ProgressBar currentStep={currentStep} totalSteps={totalSteps} />
+      <div className={styles.blackText}>등록한 정보는 마이페이지에서 변경할 수 있습니다.</div>
       <Select
         options={options}
+        value={selectedJob}
+        onChange={handleJobChange}
         className={styles.select}
         placeholder="직무"
         theme={theme => ({
@@ -53,6 +95,8 @@ export default function RegistInfo() {
       />
       <Select
         options={options}
+        value={selectedService}
+        onChange={handleServiceChange}
         className={styles.select}
         placeholder="관심 서비스"
         theme={theme => ({
@@ -66,9 +110,8 @@ export default function RegistInfo() {
         })}
         styles={customStyles}
       />
-      <div className={styles.blackText}>등록한 정보는 마이페이지에서 변경할 수 있습니다.</div>
 
-      <button className={styles.submit} onClick={handleNextStep}>
+      <button className={styles.submit} onClick={handleConfirm}>
         확인
       </button>
     </div>
