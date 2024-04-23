c6831645fb6586d127b719cc0c83b988402dbb0c
yihoney <109622@naver.com>
2024-04-24 08:27:02 +0900

    feat: 로드맵 전체 조회 메서드 생성

--- backend/cloudy/src/main/java/com/s207/cloudy/domain/roadmapgroup/roadmap/application/RoadmapService.java
+++ backend/cloudy/src/main/java/com/s207/cloudy/domain/roadmapgroup/roadmap/application/RoadmapService.java
@@ -1,2 +1,8 @@
-package com.s207.cloudy.domain.roadmapgroup.roadmap.application;public class RoadmapService {
+package com.s207.cloudy.domain.roadmapgroup.roadmap.application;
+
+import com.s207.cloudy.domain.roadmapgroup.roadmap.dto.RoadmapListRes;
+import org.springframework.data.domain.Pageable;
+
+public interface RoadmapService {
+    RoadmapListRes getRoadmapList(String job, String service, String query, Pageable pageable);
 }
