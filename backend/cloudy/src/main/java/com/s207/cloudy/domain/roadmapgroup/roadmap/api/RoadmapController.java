f9def7a1f2166fdf227c0f69dc2f022427a3c0d7
yihoney <109622@naver.com>
2024-04-24 08:29:31 +0900

    feat: 로드맵 전체 조회 컨트롤러 작성

--- backend/cloudy/src/main/java/com/s207/cloudy/domain/roadmapgroup/roadmap/api/RoadmapController.java
+++ backend/cloudy/src/main/java/com/s207/cloudy/domain/roadmapgroup/roadmap/api/RoadmapController.java
@@ -1,2 +1,32 @@
-package com.s207.cloudy.domain.roadmapgroup.roadmap.api;public class RoadmapController {
+package com.s207.cloudy.domain.roadmapgroup.roadmap.api;
+
+import com.s207.cloudy.domain.roadmapgroup.roadmap.application.RoadmapService;
+import com.s207.cloudy.domain.roadmapgroup.roadmap.dto.RoadmapListRes;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/roadmaps")
+public class RoadmapController {
+    private final RoadmapService roadmapService;
+
+    @GetMapping
+    public ResponseEntity<RoadmapListRes> getRoadmapList(@RequestParam(required = false) String job,
+                                                         @RequestParam(required = false) String service,
+                                                         @RequestParam(required = false) String query,
+                                                         @PageableDefault Pageable pageable) {
+
+        return ResponseEntity
+                .status(OK)
+                .body(roadmapService.getRoadmapList(job, service, query, pageable));
+    }
 }
