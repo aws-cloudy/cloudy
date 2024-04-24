fa49896d91e3e51e76b8b735b9e890d082d5e6f5
백민정 <peku9693@gmail.com>
2024-04-24 17:59:35 +0900

    [FE-COMPONENT] fix: create page 오류 수정

--- frontend/app/community/create/page.tsx
+++ frontend/app/community/create/page.tsx
@@ -1,27 +1,10 @@
-'use client'
-
-import { Editor, Viewer } from '@toast-ui/react-editor'
-import '@toast-ui/editor/dist/toastui-editor.css' // Editor's Style
-import { useRef } from 'react'
+import EditorComponent from '@/components/community/create/EditorComponent'
+import '@toast-ui/editor/dist/toastui-editor.css'
 
 const CommunityCreatepage = () => {
-  const editorRef = useRef<Editor>(null)
-  const viewerRef = useRef<Viewer>(null)
-
-  const handleSubmit = () => {
-    const editor = editorRef.current
-    const viewer = viewerRef.current
-    if (!editor) return
-    if (!viewer) return
-    console.log(editor.getInstance().getMarkdown())
-    viewer.getInstance().setMarkdown(editor.getInstance().getMarkdown())
-  }
-
   return (
     <>
-      <Editor ref={editorRef} initialEditType="wysiwyg" hideModeSwitch={true} />
-      <button onClick={() => handleSubmit()}>ㅇㅋ</button>
-      <Viewer ref={viewerRef} />
+      <EditorComponent />
     </>
   )
 }
