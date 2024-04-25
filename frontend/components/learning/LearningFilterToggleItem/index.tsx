6e9b111643a57dbc68f701c4b5b9bebf9d3100c0
김내림 <naerim1119@naver.com>
2024-04-25 13:45:08 +0900

    [FE-COMPONENT] refactor: 필더 설정 hook 구현 #25

--- frontend/components/learning/LearningFilterToggleItem/index.tsx
+++ frontend/components/learning/LearningFilterToggleItem/index.tsx
@@ -3,44 +3,25 @@ import styles from './LearningFilterToggleItem.module.scss'
 import { FiPlus } from 'react-icons/fi'
 import { FiMinus } from 'react-icons/fi'
 import { IFilter } from '@/types/learning'
-import { useLearningActions } from '@/stores/learning'
 import { useDifficultyFilter, useServiceFilter, useTypeFilter, usejobFilter } from '@/stores/learning'
+import { useSetFilter } from '@/hooks/useSetFilter'
 
 const LearningFilterToggoleItem = (props: { item: IFilter }) => {
   const { item } = props
 
-  const { setJobFilter, setServiceFilter, setTypeFilter, setDifficultyFilter } = useLearningActions()
   const jobs = usejobFilter()
   const services = useServiceFilter()
   const types = useTypeFilter()
   const dificulties = useDifficultyFilter()
 
-  // 있다면 제거, 없다면  추가
-  const onClick = (v: IFilter) => {
-    switch (v.category) {
-      case 'job':
-        setJobFilter(v)
-        break
-      case 'service':
-        setServiceFilter(v)
-        break
-      case 'type':
-        setTypeFilter(v)
-        break
-      case 'difficulty':
-        setDifficultyFilter(v)
-        break
-    }
-  }
+  const setFilter = useSetFilter(item)
 
   return (
     <div className={styles.container}>
-      {[...jobs, ...services, ...types, ...dificulties].every(v => v.name !== item.name) ? (
-        <FiPlus onClick={e => onClick(item)} />
-      ) : (
-        <FiMinus onClick={e => onClick(item)} />
-      )}
-      <span>{item.name}</span>
+      <div onClick={e => setFilter()} className={styles.wrap}>
+        {[...jobs, ...services, ...types, ...dificulties].every(v => v.name !== item.name) ? <FiPlus /> : <FiMinus />}
+        <span>{item.name}</span>
+      </div>
     </div>
   )
 }
--- frontend/components/learning/LearningTagListItem/index.tsx
+++ frontend/components/learning/LearningTagListItem/index.tsx
@@ -2,32 +2,15 @@ import React from 'react'
 import styles from './LearningTagListItem.module.scss'
 import { FaCheck } from 'react-icons/fa6'
 import { IFilter } from '@/types/learning'
-import { useLearningActions } from '@/stores/learning'
+import { useSetFilter } from '@/hooks/useSetFilter'
 
 const LearningTagListItem = (props: { item: IFilter }) => {
   const { item } = props
 
-  const { setJobFilter, setServiceFilter, setTypeFilter, setDifficultyFilter } = useLearningActions()
-
-  const onClick = () => {
-    switch (item.category) {
-      case 'job':
-        setJobFilter(item)
-        break
-      case 'service':
-        setServiceFilter(item)
-        break
-      case 'type':
-        setTypeFilter(item)
-        break
-      case 'difficulty':
-        setDifficultyFilter(item)
-        break
-    }
-  }
+  const setFilter = useSetFilter(item)
 
   return (
-    <div className={styles.container} onClick={e => onClick()}>
+    <div className={styles.container} onClick={e => setFilter()}>
       <FaCheck color="#FF9900" />
       <span>{item.name}</span>
     </div>
