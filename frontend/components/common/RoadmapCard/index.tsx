a5c35443ee8896cac7b0f4c5c1c40ba744b781b6
김내림 <naerim1119@naver.com>
2024-04-26 17:47:24 +0900

    [FE-COMPONENT] feat: 로드맵 페이지 구현 #32

--- frontend/components/common/RoadmapCard/index.tsx
+++ frontend/components/common/RoadmapCard/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useState } from 'react'
 import styles from './RoadmapCard.module.scss'
 import Image from 'next/image'
--- frontend/components/roadmap/main/RoadmapListSection/index.tsx
+++ frontend/components/roadmap/main/RoadmapListSection/index.tsx
@@ -1,7 +1,60 @@
 import React from 'react'
+import roadmapgreen from '@/public/img/roadmap/roadmapgreen.jpg'
+import roadmapblue from '@/public/img/roadmap/roadmapblue.jpg'
+import RoadmapCard from '@/components/common/RoadmapCard'
+import styles from './RoadmapListSection.module.scss'
+
+const rData = [
+  {
+    id: 1,
+    image: roadmapgreen,
+    status: 'scrap',
+    title: 'AWS Skill Builder Learner Guide AWS Skill Builder Learner Guide AWS Skill Builder Learner Guide Guide ',
+    context:
+      '효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음 효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 보여준다.',
+    tags: ['DataWrangler', 'GeneratedAI'],
+    comments: 2,
+  },
+  {
+    id: 2,
+    image: roadmapblue,
+    status: 'scrap',
+    title: 'AWS Skill Builder Learner Guide AWS Skill Builder Learner Guide AWS Skill Builder Learner Guide Guide ',
+    context:
+      '효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음 효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 보여준다.',
+    tags: ['DataWrangler', 'GeneratedAI'],
+    comments: 2,
+  },
+  {
+    id: 3,
+    image: roadmapgreen,
+    status: 'scrap',
+    title: 'AWS Skill Builder Learner Guide AWS Skill Builder Learner Guide AWS Skill Builder Learner Guide Guide ',
+    context:
+      '효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음 효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 보여준다.',
+    tags: ['DataWrangler', 'GeneratedAI'],
+    comments: 2,
+  },
+  {
+    id: 4,
+    image: roadmapgreen,
+    status: 'scrap',
+    title: 'AWS Skill Builder Learner Guide AWS Skill Builder Learner Guide AWS Skill Builder Learner Guide Guide ',
+    context:
+      '효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 알 수 있음 효과적인 프롬포트를 설계하기 위한 원칙, 기법 및 모범 사례를 보여준다.',
+    tags: ['DataWrangler', 'GeneratedAI'],
+    comments: 2,
+  },
+]
 
 const RoadmapListSection = () => {
-  return <div>RoadmapListSection</div>
+  return (
+    <div className={styles.container}>
+      {rData.map((road: any) => (
+        <RoadmapCard road={road} key={road.id} />
+      ))}
+    </div>
+  )
 }
 
 export default RoadmapListSection
