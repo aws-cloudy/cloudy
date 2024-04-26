83d0e55a683a7b26a52f760f3152568e4450cc43
김내림 <naerim1119@naver.com>
2024-04-26 10:32:16 +0900

    [FE-PAGE] feat: 로드맵 페이지 생성 #31

--- frontend/components/common/Layout/index.tsx
+++ frontend/components/common/Layout/index.tsx
@@ -1,7 +1,7 @@
-import React, { ReactNode } from 'react'
+import React from 'react'
 import styles from './Layout.module.scss'
 
-const Layout = ({ children }: { children: ReactNode }) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return <section className={styles.section}>{children}</section>
 }
 
