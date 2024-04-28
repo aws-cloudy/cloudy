e40ff675c6563d9b7ea4f540a2dba633011544cc
naerim <naerim1119@gmail.com>
2024-04-28 19:49:40 +0900

    [FE-COMPONENT] style: 학습페이지 컴포넌트 스타일 수정 #36

--- frontend/components/common/LearningItem/LearningItem.stories.tsx
+++ frontend/components/common/LearningItem/LearningItem.stories.tsx
@@ -1,10 +1,17 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import LearningItem from './'
 import { learningData } from '@/components/learning/LearningList/learningData'
+import { ILearningItem } from '@/types/learning'
+
+const LearningItemComponent = ({ item, layout }: { item: ILearningItem; layout: string }) => (
+  <div style={{ width: layout === 'justify' ? '100%' : 250 }}>
+    <LearningItem item={item} layout={layout} />
+  </div>
+)
 
 const meta = {
   title: 'common/LearningItem',
-  component: LearningItem,
+  component: LearningItemComponent,
   parameters: {
     layout: 'fullscreen',
   },
@@ -17,4 +24,8 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Item: Story = {}
+export const GridItem: Story = {}
+
+export const JustifyItem: Story = {
+  args: { layout: 'justify' },
+}
--- frontend/components/learning/LearningList/index.tsx
+++ frontend/components/learning/LearningList/index.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React, { useEffect, useState } from 'react'
 import styles from './LearningList.module.scss'
 import { learningData } from './learningData'
