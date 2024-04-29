3991f3d2a2726182effc185b45fb072d510b334b
yihoney <109622@naver.com>
2024-04-29 15:07:04 +0900

    [BE] test: 회원별 로드맵 조회 컨트롤러 테스트 작성

--- backend/cloudy/src/test/java/com/s207/cloudy/domain/roadmap_group/member/api/MemberRoadmapControllerTest.java
+++ backend/cloudy/src/test/java/com/s207/cloudy/domain/roadmap_group/member/api/MemberRoadmapControllerTest.java
@@ -1,26 +1,34 @@
 package com.s207.cloudy.domain.roadmap_group.member.api;
 
-import com.s207.cloudy.global.auth.api.AuthController;
-import com.s207.cloudy.global.auth.error.exception.AuthorizationException;
-import com.s207.cloudy.global.auth.service.JwtService;
-import com.s207.cloudy.global.config.SecurityConfig;
+import com.s207.cloudy.domain.members.entity.Member;
+import com.s207.cloudy.domain.roadmap_group.member.application.MemberRoadmapService;
+import com.s207.cloudy.domain.roadmap_group.roadmap.application.RoadmapService;
+import com.s207.cloudy.domain.roadmap_group.roadmap.domain.Roadmap;
+import com.s207.cloudy.domain.roadmap_group.roadmap.dto.RoadmapListRes;
+import com.s207.cloudy.domain.roadmap_group.roadmap.dto.RoadmapRes;
+import com.s207.cloudy.dummy.DummyRoadmap;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@WebMvcTest(value = {MemberRoadmapController.class, AuthController.class},
-        includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class))
+@WebMvcTest(controllers = MemberRoadmapController.class,
+        excludeAutoConfiguration = SecurityAutoConfiguration.class)
 class MemberRoadmapControllerTest {
 
 
@@ -28,24 +36,59 @@ class MemberRoadmapControllerTest {
     private MockMvc mockMvc;
 
 
+    @Autowired
+    MemberRoadmapController memberRoadmapController;
+
     @MockBean
-    private JwtService jwtService;
+    MemberRoadmapService mockMemberRoadmapService;
 
+    @MockBean
+    RoadmapService mockRoadmapService;
 
+    Roadmap dummyRoadmap;
+    RoadmapRes dummyRoadmapRes1;
+    RoadmapRes dummyRoadmapRes2;
 
-    @Test
-    @DisplayName("로그인이 되어있지 않다면 403 에러를 반환한다.")
-    void should_401_when_not_login() throws Exception{
-
-        given(
-                jwtService.isTokenValid(any())
-        ).willThrow(AuthorizationException.class);
-
-        mockMvc.perform(get("/api/v1/roadmaps/my"))
-                .andExpect(status().isForbidden());
+    @BeforeEach
+    void setUp() {
+        dummyRoadmap = DummyRoadmap.getDummyRoadmap();
+        dummyRoadmapRes1 = DummyRoadmap.getDummyRoadmapRes(dummyRoadmap);
+        dummyRoadmapRes2 = DummyRoadmap.getDummyRoadmapRes(dummyRoadmap);
     }
 
 
+    @Test
+    @DisplayName("회원이 북마크한 로드맵 리스트를 정상적으로 조회해 반환하고, 200 OK를 반환한다.")
+    void should_200_when_get_member_bookmarklist() throws Exception {
+
+        List<RoadmapRes> dummyList = List.of(dummyRoadmapRes1, dummyRoadmapRes2);
+
+        given(mockMemberRoadmapService.getRoadmapList(any(Member.class)))
+                .willReturn(new RoadmapListRes(dummyList, dummyList.size()));
+
+        mockMvc.perform(get("/api/v1/my/roadmaps")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.roadmaps[0].roadmapId",
+                        equalTo(dummyRoadmapRes1.getRoadmapId()), Integer.class))
+                .andExpect(jsonPath("$.roadmaps[0].title",
+                        equalTo(dummyRoadmapRes1.getTitle()), String.class))
+                .andExpect(jsonPath("$.roadmaps[0].thumbnail",
+                        equalTo(dummyRoadmapRes1.getThumbnail()), String.class))
+                .andExpect(jsonPath("$.roadmaps[0].service",
+                        equalTo(dummyRoadmapRes1.getService()), String.class))
+                .andExpect(jsonPath("$.roadmaps[0].job",
+                        equalTo(dummyRoadmapRes1.getJob()), String.class))
+                .andExpect(jsonPath("$.roadmaps[0].summary",
+                        equalTo(dummyRoadmapRes1.getSummary()), String.class))
+                .andExpect(jsonPath("$.roadmaps[0].commentsCnt",
+                        equalTo(dummyRoadmapRes1.getCommentsCnt()), Long.class))
+                .andExpect(jsonPath("$.totalPageCnt",
+                        equalTo((long) dummyList.size()), Long.class))
+                .andDo(print());
+    }
 
 
 }
