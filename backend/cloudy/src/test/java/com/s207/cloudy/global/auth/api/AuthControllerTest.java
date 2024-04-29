e972eaa027f927d8bd7c4b485f2a9e93be6ca4c5
yihoney <109622@naver.com>
2024-04-29 15:06:24 +0900

    [BE] test: url별 권한 체킹 테스트 생성

---
+++ backend/cloudy/src/test/java/com/s207/cloudy/global/auth/api/AuthControllerTest.java
@@ -0,0 +1,43 @@
+package com.s207.cloudy.global.auth.api;
+
+import com.s207.cloudy.global.auth.error.exception.AuthorizationException;
+import com.s207.cloudy.global.auth.service.JwtService;
+import com.s207.cloudy.global.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(value = {AuthController.class},
+        includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class))
+class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private JwtService jwtService;
+
+    @Test
+    @DisplayName("로그인이 되어있지 않은 회원이 마이페이지를 접근하면 403 에러를 반환한다.")
+    void should_401_when_not_login_user_access_mypage() throws Exception {
+
+        given(
+                jwtService.isTokenValid(any())
+        ).willThrow(AuthorizationException.class);
+
+        mockMvc.perform(get("/api/v1/roadmaps/my"))
+                .andExpect(status().isForbidden());
+    }
+
+
+}
