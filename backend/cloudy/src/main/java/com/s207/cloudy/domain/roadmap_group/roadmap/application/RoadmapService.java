7a360dfb8c170508d24da4a1863d49a2231d58b6
yihoney <109622@naver.com>
2024-04-29 14:27:45 +0900

    [BE] feat: 로드맵 엔터티 찾는 서비스 메서드 구현
    - 존재하지 않는 로드맵일 경우 터뜨릴 예외 클래스 생성

--- backend/cloudy/src/main/java/com/s207/cloudy/domain/roadmap_group/roadmap/application/RoadmapService.java
+++ backend/cloudy/src/main/java/com/s207/cloudy/domain/roadmap_group/roadmap/application/RoadmapService.java
@@ -1,8 +1,15 @@
 package com.s207.cloudy.domain.roadmap_group.roadmap.application;
 
+import com.s207.cloudy.domain.roadmap_group.roadmap.domain.Roadmap;
 import com.s207.cloudy.domain.roadmap_group.roadmap.dto.RoadmapListRes;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface RoadmapService {
     RoadmapListRes getRoadmapList(String job, String service, String query, Pageable pageable);
+
+    RoadmapListRes findMemberRoadmapList(List<Integer> memberRoadmapList);
+
+    Roadmap findRoadmapEntity(int roadmapId);
 }
