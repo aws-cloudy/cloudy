0018daed9ec31c96fc5745992b8846599a9d0565
김내림 <naerim1119@naver.com>
2024-04-30 10:16:11 +0900

    [FE-PAGE] feat: favicon 설정 #42

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -12,6 +12,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Cloudy',
   description: 'AWS 클라우드 서비스를 친구처럼 편안하게 Cloudy',
+  icons: {
+    icon: '/icon/favicon.png',
+  },
 }
 
 export default function RootLayout({
