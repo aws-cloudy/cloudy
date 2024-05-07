5ac14b6aacd9b1e9be230cd46d582de162bb41a2
전상혁 <tkdgur0826@gmail.com>
2024-05-07 16:42:40 +0900

    [FE-SETTING] fix: 신규 가입자 플래그 주석처리 #12

--- frontend/utils/authOptions.tsx
+++ frontend/utils/authOptions.tsx
@@ -69,7 +69,7 @@ export const authOptions: NextAuthOptions = {
       return session
     },
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_SECRET,
   pages: {
     newUser: '/join',
   },
