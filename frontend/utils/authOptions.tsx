22751215a190b493d3120781283b5c99ecc467b7
전상혁 <tkdgur0826@gmail.com>
2024-05-07 15:21:20 +0900

    [FE-SETTING] fix: 회원가입 로직 임시 주석처리 #12

--- frontend/utils/authOptions.tsx
+++ frontend/utils/authOptions.tsx
@@ -1,7 +1,7 @@
 import GoogleProvider from 'next-auth/providers/google'
 import CognitoProvider from 'next-auth/providers/cognito'
 import { NextAuthOptions } from 'next-auth'
-import { checkUserExists,  } from './cognito'
+import { checkUserExists } from './cognito'
 
 export const authOptions: NextAuthOptions = {
   //Providers 소셜 로그인 서비스 코드
@@ -12,47 +12,52 @@ export const authOptions: NextAuthOptions = {
       issuer: process.env.COGNITO_ISSUER,
       idToken: true,
       checks: 'nonce',
-      
     }),
   ],
   callbacks: {
-    async signIn({user, profile}:any){
-      let username;
+    async signIn({ user, profile }: any) {
+      let username
       if (profile?.identities) {
         // 구글 로그인의 경우, Cognito에 등록된 Username 사용
-        username = `google_${profile.identities[0].userId}`;
+        username = `google_${profile.identities[0].userId}`
       } else {
         // 일반 Cognito 사용자의 경우 이메일을 사용
-        username = user.email;
+        username = user.email
       }
-      if(!profile.job_id){
-        user.isNewUser = false;
-        return `/join`;
-      }
-      return true;
+      user.username = username
+      // const { exists, hasJobId } = await checkUserExists(username)
+      console.log(user)
+      // if (!exists || !hasJobId) {
+      // user.isNewUser = false
+
+      // return `/join`
+      // return true
+      // }
+      return true
     },
 
     // 토큰을 세션에 추가하는 콜백
     async jwt({ token, user, account, profile }: any) {
       if (user) {
+        // token.username = user.username
         // const userExists  = await checkUserExists(user.email);
         // token.isNewUser = !userExists ;  // 존재하지 않는 사용자라면 true
         // 프로필에서 job_id, service_id를 추출해 JWT 토큰에 추가
-        token.jobId = user?.jobId || profile?.job_id;
-        token.serviceId = user?.serviceId || profile?.service_id;
-        token.user = user
+        token.jobId = user?.jobId || profile?.job_id
+        token.serviceId = user?.serviceId || profile?.service_id
         token.account = account
         token.profile = profile
         token.accessToken = account.id_token
         token.id = account.providerAccountId
       }
+
       return token
     },
+
     // 세션 데이터에 accessToken 추가
     async session({ session, token }: any) {
-      
-      if (token){
-        session.user.isNewUser = token.isNewUser
+      if (token) {
+        session.user.username = token.username
         session.accessToken = token.accessToken as string
         session.user.id = token.id as string
         session.user.uuid = token.sub as string
@@ -60,11 +65,12 @@ export const authOptions: NextAuthOptions = {
         session.user.jobId = token.jobId
         session.user.serviceId = token.serviceId
       }
+
       return session
     },
   },
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
-    newUser: '/join'
-  }
+    newUser: '/join',
+  },
 }
--- frontend/utils/cognito.ts
+++ frontend/utils/cognito.ts
@@ -1,27 +1,27 @@
-import { AdminGetUserCommand, CognitoIdentityProviderClient } from '@aws-sdk/client-cognito-identity-provider';
+import { AdminGetUserCommand, CognitoIdentityProviderClient } from '@aws-sdk/client-cognito-identity-provider'
 
 const client = new CognitoIdentityProviderClient({
   region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string
-  }
-});
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+  },
+})
 
-
-export async function checkUserExists(username: string): Promise<boolean> {
+export async function checkUserExists(username: string): Promise<{ exists: boolean; hasJobId: boolean }> {
   const command = new AdminGetUserCommand({
     UserPoolId: process.env.COGNITO_USER_POOL_ID as string,
-    Username: username
-  });
+    Username: username,
+  })
 
   try {
-    await client.send(command);
-    return true;
-  } catch (error:any) {
+    const response = await client.send(command)
+    const hasJobId = response.UserAttributes?.some(attr => attr.Name === 'custom:job_id' && attr.Value)
+    return { exists: true, hasJobId: !!hasJobId }
+  } catch (error: any) {
     if (error.name === 'UserNotFoundException') {
-      return false;
+      return { exists: false, hasJobId: false }
     }
-    throw error;
+    throw error
   }
 }
