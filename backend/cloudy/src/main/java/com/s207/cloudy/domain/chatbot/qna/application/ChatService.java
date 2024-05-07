8fbe6ae4325f7de7e531fb2c046705512967c5c4
yihoney <109622@naver.com>
2024-05-07 15:32:10 +0900

    [BE] remove: 패키지 변경된 클래스 제거

--- backend/cloudy/src/main/java/com/s207/cloudy/domain/chatbot/qna/application/ChatService.java
+++
@@ -1,7 +0,0 @@
-package com.s207.cloudy.domain.chatbot.qna.application;
-
-import java.util.Map;
-
-public interface ChatService {
-    void generateStreamingChat(String template, Map<String, Object> variables);
-}
--- backend/cloudy/src/main/java/com/s207/cloudy/domain/chatbot/qna/application/EmbeddingService.java
+++
@@ -1,9 +0,0 @@
-package com.s207.cloudy.domain.chatbot.qna.application;
-
-import dev.langchain4j.data.embedding.Embedding;
-
-public interface EmbeddingService {
-    void initOpenAiEmbeddingModel(String modelName);
-
-    Embedding getEmbeddingQuery(String query);
-}
