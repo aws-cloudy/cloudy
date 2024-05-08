bbe6250d0418c546bf3a7f6c48149f1e32d81d72
전상혁 <tkdgur0826@gmail.com>
2024-05-08 13:21:56 +0900

    [FE-SETTING] feat: authenticated 상태 조건 추가 #12

--- frontend/app/join/page.tsx
+++ frontend/app/join/page.tsx
@@ -1,11 +1,22 @@
 'use client'
 import RegistInfomation from '@/components/RegistInfomation'
 import styles from './page.module.scss'
-import { useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useSession } from 'next-auth/react'
+import { useEffect } from 'react'
 
 const joinPage = () => {
+  const { data: session, status } = useSession()
+  const router = useRouter()
   const searchParams = useSearchParams()
   const username = searchParams.get('auth')
+
+  useEffect(() => {
+    if (status === 'authenticated') {
+      alert('비정상적인 접근입니다.')
+      router.push('/')
+    }
+  }, [status, router])
 
   return (
     <>
