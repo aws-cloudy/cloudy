8cb07ec5923b2b1fe69876e317fe5d63cad278c8
김내림 <naerim1119@naver.com>
2024-05-08 15:28:29 +0900

    [FE-PAGE] fix: learning 페이지 테스트코드, storybook 에러 수정 #63

--- frontend/components/learning/LearningInput/index.test.tsx
+++ frontend/components/learning/LearningInput/index.test.tsx
@@ -1,29 +1,41 @@
 import { fireEvent, render, screen } from '@testing-library/react'
 import LearningInput from '.'
+import { useLearningActions } from '@/stores/learning'
+
+jest.mock('@/stores/learning', () => ({
+  useLearningActions: jest.fn(),
+  useLearningKeyword: () => '',
+}))
 
 describe('LearningInput', () => {
+  let mockSetKeyword
+  beforeEach(() => {
+    // beforeEach 블록에서 모든 테스트 전에 mock 함수를 설정
+    mockSetKeyword = jest.fn()
+    ;(useLearningActions as jest.Mock).mockReturnValue({ setKeyword: mockSetKeyword })
+  })
+
   it('input value가 변경되었을때 값이 올바르게 업데이트되는지 확인합니다', () => {
     const mockSetValue = jest.fn()
-    const mockSetKeyword = jest.fn()
 
-    render(<LearningInput value="" setValue={mockSetValue} keyword="" setKeyword={mockSetKeyword} />)
+    render(<LearningInput value="" setValue={mockSetValue} />)
 
     const input = screen.getByPlaceholderText('무엇이든 검색해보세요 ...')
     fireEvent.change(input, { target: { value: 'test' } })
 
     expect(mockSetValue).toHaveBeenCalledWith('test')
-    expect(mockSetKeyword).toHaveBeenCalledWith('')
   })
 
   it('Enter를 눌렀을때 handleSubmit 함수가 호출되는지 확인합니다', () => {
     const mockSetValue = jest.fn()
-    const mockSetKeyword = jest.fn()
 
-    render(<LearningInput value="test" setValue={mockSetValue} keyword="" setKeyword={mockSetKeyword} />)
+    const { setKeyword } = useLearningActions()
+
+    render(<LearningInput value="test" setValue={mockSetValue} />)
 
     const input = screen.getByPlaceholderText('무엇이든 검색해보세요 ...')
     fireEvent.keyDown(input, { key: 'Enter', code: 'Enter' })
 
-    expect(mockSetKeyword).toHaveBeenCalledWith('test')
+    expect(setKeyword).toHaveBeenCalledWith('test')
   })
 })
