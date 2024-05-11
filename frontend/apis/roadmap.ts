dbf7c97bad76701a6265646c66d97b4bc42350c4
naerim <naerim1119@gmail.com>
2024-05-12 04:58:06 +0900

    [FE-PAGE] feat: 필터 searchparam 적용 #85

--- frontend/apis/roadmap.ts
+++ frontend/apis/roadmap.ts
@@ -6,13 +6,13 @@ const url = '/roadmaps'
 
 // 로드맵 전체 조회
 export const getRoadmaps = async (offset: number, query: string, job: string, service: string) => {
-  const response = await client
-    .get(`${url}?page=${offset}&size=${ROADMAP_ROWS_PER_PAGE}&query=${query}&job=${job}&service=${service}`)
-    .then(res => res.data.roadmaps)
-    .catch(err => {
-      console.log(err)
-    })
-  return response
+  // const response = await client
+  //   .get(`${url}?page=${offset}&size=${ROADMAP_ROWS_PER_PAGE}&query=${query}&job=${job}&service=${service}`)
+  //   .then(res => res.data.roadmaps)
+  //   .catch(err => {
+  //     console.log(err)
+  //   })
+  return []
 }
 
 // 로드맵 상세 조회
