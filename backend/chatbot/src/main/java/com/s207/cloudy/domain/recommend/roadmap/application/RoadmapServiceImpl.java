9fe1a75f696c12dcc4eacfbea1e430fd64bce909
the_1_week <the_1_week@naver.com>
2024-05-13 00:55:21 +0900

    [BE-chatbot] add : 로드맵 추천 챗봇 개발 #90

--- backend/chatbot/src/main/java/com/s207/cloudy/domain/recommend/roadmap/application/RoadmapServiceImpl.java
+++ backend/chatbot/src/main/java/com/s207/cloudy/domain/recommend/roadmap/application/RoadmapServiceImpl.java
@@ -1,7 +1,11 @@
 package com.s207.cloudy.domain.recommend.roadmap.application;
 
+import com.s207.cloudy.domain.chatbot.common.dto.ChatReq;
+import com.s207.cloudy.domain.chatbot.common.entity.Chatbot;
+import com.s207.cloudy.domain.recommend.exception.RecommendException;
 import com.s207.cloudy.domain.recommend.roadmap.dao.RoadmapRepository;
 import com.s207.cloudy.domain.recommend.roadmap.dto.RoadmapListRes;
+import com.s207.cloudy.global.error.enums.ErrorCode;
 import com.s207.cloudy.global.infra.chatmodel.OpenAiChatService;
 import com.s207.cloudy.global.infra.embeddingmodel.OpenAiEmbeddingService;
 import com.s207.cloudy.global.infra.embeddingstore.EmbeddingStoreService;
@@ -11,9 +15,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static com.s207.cloudy.global.infra.embeddingstore.enums.IndexName.CLOUDY;
@@ -79,6 +86,49 @@ public class RoadmapServiceImpl {
                 .build();
 
     }
+
+    public Flux<String> generateChatStream(ChatReq chatReq, String userId) {
+        String keywords = openAiChatService.extractKeywords(chatReq.getInputData())
+                .replace("AWS", "")
+                .replace("Amazon", "");
+
+        List<Integer> recommendedRoadmapIds = getRecommendedRoadmapIds(keywords, 1);
+
+        var recommendedRoadmap = roadmapRepository
+                .findById(recommendedRoadmapIds.get(0))
+                .orElseThrow(()-> new RecommendException(ErrorCode.NOT_FOUND));
+
+        String template = """ 
+                아래의 상황을 참고해 질문에 대한 답을 작성해줘.
+                
+                상황: aws 강의에 대한 추천 결과는 다음과 같아.
+                    title : {{title}}
+                    desc : {{desc}} 
+                     강의 정보(desc)와 질문을 바탕으로 추천한 이유를 만들어줘 
+                     마지막은 링크를 생성해야해 
+                     http://aws-cloudy/roadmap?query=
+                     링크 양식은 다음과 같고 query뒤에는 {{title}}을 붙이면 돼. 
+                     링크를 만들어 낼때만 {{title}}에 space가 있을 경우 전부 %로 치환해야해 
+                    최종 답변 양식은 다음과 같아 
+                    {{title}}을 추천 드려요!
+                    추천 드리는 이유는 다음과 같아요 
+                    (추천 이유 설명하기)  
+                    [바로가기](만들어낸 링크를 바탕으로 하이퍼 링크 형태로 만들기) 
+                    하이퍼 링크의 []안의 내용은 바로가기로 고정해줘 
+                    
+                    
+                질문: {{input}}
+                답변:
+                """;
+
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("title", recommendedRoadmap.getTitle());
+        variables.put("desc", recommendedRoadmap.getDesc());
+        variables.put("input", chatReq.getInputData());
+
+        return openAiChatService.generateStreamingChat(template, variables, userId, Chatbot.ROADMAP);
+    }
+
 
 
 }
