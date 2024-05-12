4141e3b3e3b09e2da87d847914445e1c58ad8551
the_1_week <the_1_week@naver.com>
2024-05-13 00:46:15 +0900

    [BE-chatbot] add : 로드맵 추천 API #90

--- backend/chatbot/src/main/java/com/s207/cloudy/global/infra/chatmodel/ChatService.java
+++ backend/chatbot/src/main/java/com/s207/cloudy/global/infra/chatmodel/ChatService.java
@@ -6,6 +6,8 @@ import reactor.core.publisher.Flux;
 import java.util.Map;
 
 public interface ChatService {
+
+    String extractKeywords(String query);
     Flux<String> generateStreamingChat(String template, Map<String, Object> variables, String userId, Chatbot chatbot);
 
     String generateChat(String template, Map<String, Object> variables);
