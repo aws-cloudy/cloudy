7e54aa95c477a53c1ae0a3d9b2ad96ae93a6060e
naerim <naerim1119@gmail.com>
2024-05-12 18:24:05 +0900

    [FE-COMPONENT] feat: 로드맵 추천 컴포넌트 설명 추가 #84

--- frontend/components/roadmap/main/RoadmapRecommend/index.tsx
+++ frontend/components/roadmap/main/RoadmapRecommend/index.tsx
@@ -5,6 +5,7 @@ import styles from './RoadmapRecommend.module.scss'
 import { roadmapData } from '../RoadmapListSection/roadmapData'
 import RoadmapRecommendItem from '../RoadmapRecommendItem'
 import { useSearchParams } from 'next/navigation'
+import { MdInfoOutline } from 'react-icons/md'
 
 const RoadmapRecommend = () => {
   const params = useSearchParams()
@@ -13,7 +14,13 @@ const RoadmapRecommend = () => {
   if (!keyword) return
   return (
     <article className={styles.container}>
-      <div className={styles.title}>존 Pick!</div>
+      <div className={styles.title}>
+        <div>존 Pick!</div>
+        <div className={styles.infoWrap}>
+          <MdInfoOutline color="#ff9900" cursor="pointer" />
+          <p className={styles.arrowBox}>존은 로드맵을 추천해주는 클라우디의 서비스입니다</p>
+        </div>
+      </div>
       <div className={styles.desc}>{`'${keyword}'와 관련된 추천 로드맵입니다`}</div>
       <div className={styles.wrap}>
         {roadmapData.map(item => (
