f4cb816d2db03d752681984fef385fa97b5885b1
김내림 <naerim1119@naver.com>
2024-05-14 12:17:23 +0900

    [FE-PAGE] feat: 추천 로드맵 클릭 이벤트 추가 #96

--- frontend/components/roadmap/main/RoadmapRecommendItem/index.tsx
+++ frontend/components/roadmap/main/RoadmapRecommendItem/index.tsx
@@ -3,10 +3,15 @@ import styles from './RoadmapRecommendItem.module.scss'
 import { IRoadmapCard } from '@/types/roadmap'
 import Image from 'next/image'
 import { getShortText } from '@/utils/getShortText'
+import { useRouter } from 'next/navigation'
 
 const RoadmapRecommendItem = ({ item }: { item: IRoadmapCard }) => {
+  const router = useRouter()
+
+  const onClick = () => router.push(`/roadmap/${item.roadmapId}`)
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={onClick}>
       <Image src={item.thumbnail} alt={item.title} fill priority sizes="auto" />
       <div className={styles.hoverWrap}>
         <div className={styles.title}>{item.title}</div>
