02d59836bc67a17b0665c6d038fd5b6eb70b9350
백민정 <peku9693@gmail.com>
2024-05-14 10:30:33 +0900

    [FE-PAGE] fix: API 주소 수정 #89

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -53,8 +53,8 @@ const nextConfig = {
         destination: 'https://o5u2oz980k.execute-api.ap-northeast-2.amazonaws.com/prod/api/v1/:path*',
       },
       {
-        source: '/cloudy-chat-api',
-        destination: 'http://k10s207.p.ssafy.io:8080/api/v1/chats',
+        source: '/cloudy-chat-api/:path*',
+        destination: 'https://k10s207.p.ssafy.io/api/v1/:path*',
       },
     ]
   },
