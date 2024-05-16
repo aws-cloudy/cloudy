91ca49af0caedcc9e3964f79571543f4bda671ba
yihoney <109622@naver.com>
2024-05-16 13:50:31 +0900

    [BE-catalog]  fix: 로드맵 전체 조회 시 필터링 오류 해결
    - 언더바 포함 시 띄어쓰기로 변환

--- backend/cloudy/src/main/java/com/s207/cloudy/domain/roadmap_group/roadmap/application/RoadmapService.java
+++ backend/cloudy/src/main/java/com/s207/cloudy/domain/roadmap_group/roadmap/application/RoadmapService.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Pageable;
 public interface RoadmapService {
     RoadmapListRes findRoadmapList(String job, String service, String query, Pageable pageable);
 
+    String replaceUnderBarToWhiteSpace(String param);
+
     Roadmap findRoadmapEntity(int roadmapId);
 
     RoadmapDetailsRes getRoadmapDetails(Integer roadmapId);
